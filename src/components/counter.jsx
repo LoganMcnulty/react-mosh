dae3b6a99f4ff0521a79c68c4ba97d9fefe354a0
Logan McNulty <52426539+LoganMcnulty@users.noreply.github.com>
2021-03-26 10:55:41 -0400

    yo yo

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state ={
-        count:0,
-        moonURL: 'https://www.thisiscolossal.com/wp-content/uploads/2019/02/moon-og.jpg',
-        tags:['tag1', 'tag2', 'tag3']
+        value: this.props.value
     }
     styles = {
         span:{
@@ -18,7 +16,7 @@ class Counter extends Component {
 
     handleIncrement = (product) => {
         console.log(product)
-        this.setState({count: this.state.count + 1})
+        this.setState({value: this.state.value + 1})
     }
 
     render() {
@@ -31,7 +29,7 @@ class Counter extends Component {
                         <button onClick={
                             () => this.handleIncrement({id:1})
                             } 
-                            className='btn btn-primary'>Increment</button>
+                            className='btn btn-primary m-2'>Increment</button>
                     </div>
                 </div>
             </div>
@@ -40,14 +38,14 @@ class Counter extends Component {
     }
 
     getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
+        let classes = "badge m-2 p-2 badge-";
+        classes += (this.state.value === 0) ? "warning" : "primary";
         return classes;
     }
 
     formatCount(){
-        const {count} = this.state
-        return count === 0 ? 'Zero' : count
+        const {value} = this.state
+        return value === 0 ? 'Zero' : value
     }
 }
 
